1425b91197fbe9d00ba37ca9d91b0f7b8f3d6942
shreehari-divate <shreeharidivate03@gmail.com>
2023-11-11 13:43:26 +0530

    update1.1

--- detection.py
+++ detection.py
@@ -4,13 +4,13 @@ import numpy as np
 import math
 
 classnames = ['Ambulance', 'Bus', 'Car', 'Motorcycle', 'Truck']
-model = YOLO(r'best.pt')
+model = YOLO('best.pt')
 cam = cv2.VideoCapture('vehicle_vid3.mp4')
 
 fsize = (int(cam.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 fps = int(cam.get(cv2.CAP_PROP_FPS))
-fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-output_video = cv2.VideoWriter('Result.mp4', fourcc, fps, fsize)
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+res_video = cv2.VideoWriter('Result.mp4', fourcc, fps, fsize)
 
 while True:
 
@@ -35,7 +35,7 @@ while True:
             cv2.putText(frame, f'{classnames[cls]} {conf}', (max(0, x1), max(40, y1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                         (0, 255, 0), 2)
 
-    output_video.write(frame)
+    res_video.write(frame)
     cv2.imshow("live", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
